67152fe50dc19f574ea41a9e006a0eed51e1ad06
Nicolas Seiler <nicolas.seiler@wincasa.ch>
2020-09-01 12:10:54 +0200

    Remove debug logging

--- src/NServiceBus.Transport.InMemory/InMemoryQueueCreator.cs
+++ src/NServiceBus.Transport.InMemory/InMemoryQueueCreator.cs
@@ -5,8 +5,6 @@ namespace NServiceBus.Transport.InMemory
 {
     public class InMemoryQueueCreator : ICreateQueues
     {
-        private readonly ILog log = LogManager.GetLogger<InMemoryTransport>();
-
         public InMemoryQueueCreator(InMemoryDatabase inMemoryDatabase)
         {
             InMemoryDatabase = inMemoryDatabase;
@@ -16,12 +14,6 @@ namespace NServiceBus.Transport.InMemory
 
         public Task CreateQueueIfNecessary(QueueBindings queueBindings, string identity)
         {
-            log.Info("Creating queues...");
-            var sendingAddresses = string.Join("\n", queueBindings.SendingAddresses);
-            var receivingAddresses = string.Join("\n", queueBindings.ReceivingAddresses);
-            log.Info($"Sending addresses: {sendingAddresses}");
-            log.Info($"Receiving addresses: {receivingAddresses}");
-
             foreach (var sendingAddress in queueBindings.SendingAddresses)
             {
                 InMemoryDatabase.CreateQueueIfNecessary(sendingAddress);
